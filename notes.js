a34254f182eab5f89ddb5d88fbb2be88183fa5e0
Alyssa Reibel <Alyssa@Alyssas-MacBook-Pro.local>
2023-02-07 19:23:47 -0600

    first commit

---
+++ notes.js
@@ -0,0 +1,18 @@
+/* REST
+Representational State Transfer
+developed by Roy, is not a protocol or HTTP, 
+it can be thought of as the way we should use HTTP to create
+ focuses heavily on transferring the state of a resource
+ JSON=Javascript Object Notation
+ preferred way for most developers to transfer the state of a resource.
+example of JSON
+ {
+    accountNumber: "09986345",
+    balance: 670.34,
+    accountType: "checking"
+ }
+ HTTP Verb Methods
+ create-post
+ read-get
+ update-update
+ delete-delete
